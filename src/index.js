d850a83ae6772c53294c92867fd9ed5e8eca3604
wltjsl <ljss7291@gmail.com>
2024-02-22 11:36:52 +0900

    오류 다시 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import typeorm from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
-
+console.log(process.env.USER_PASSWORD);
 const config = {
   type: "mysql",
   host: "localhost",
@@ -9,12 +9,20 @@ const config = {
   username: "root",
   password: process.env.USER_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: true,
-  logging: false,
-  entities: ["./entities/resume.entity.js", "./entities/user.entity.js"],
-  cli: {
-    entitiesDir: `./entities`
-  }
+  synchronize: false,
+  entities: ["./entities/resume.entity.js", "./entities/user.entity.js"]
 };
 
-export const dataSource = new typeorm.DataSource(config);
+const dataSource = new typeorm.DataSource(config);
+
+async function initializeApp() {
+  try {
+    await dataSource.initialize();
+    console.log("데이터베이스 연결 성공!");
+  } catch (error) {
+    console.error("데이터베이스 연결 에러", error);
+  }
+}
+initializeApp();
+
+export { dataSource };
--- src/repositories/resume.repository.js
+++ src/repositories/resume.repository.js
@@ -1,32 +1,36 @@
+import { dataSource } from "../index.js";
 import { Resumes } from "../entities/resume.entity.js";
-
 class ResumeRepository {
+  constructor() {
+    this.resumeRepository = dataSource.getRepository(Resumes);
+  }
+
   async createResume(title, content, userName) {
-    return await Resumes.create({ title, content, userName });
+    return await this.resumeRepository.create({ title, content, userName });
   }
 
   async getAllResumes() {
-    return await Resumes.findAll();
+    return await this.resumeRepository.find();
   }
 
   async getResumeById(resumeId) {
-    return await resumeId.findByPk(resumeId);
+    return await this.resumeRepository.findOne(resumeId);
   }
 
   async updateResume(resumeId, title, content, status) {
-    const resume = await resume.findByPk(resumeId);
+    const resume = await this.resumeRepository.findOne(resumeId);
     if (!resume) {
       throw new Error("이력서 조회에 실패하였습니다.");
     }
     resume.title = title;
     resume.content = content;
     resume.status = status;
-    await resume.save();
+    await this.resumeRepository.save(resume);
     return resume;
   }
 
   async deleteResume(resumeId) {
-    return await resumeId.destroy({ where: { resumeId } });
+    return await this.resumeRepository.delete(resumeId);
   }
 }
 
